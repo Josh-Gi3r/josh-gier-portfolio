c0d14520125d8bd4636b2cc647cd0afedac93bb9
Josh G <129348785+Josh-Gi3r@users.noreply.github.com>
2026-09-11 22:26:18 +0800

    Add first-week meal photography

---
+++ apps/home-meals/data/meal-assets.ts
@@ -0,0 +1,9 @@
+export const mealImages:Record<string,string>={
+ "gold-chicken-curry":"https://d8j0ntlcm91z4.cloudfront.net/user_33B38CK6rN3xZGtZllRALJXPhHT/hf_20260911_142524_0a6424bc-1df7-44a5-be8e-348a171c6acd.png",
+ "beef-broccoli":"https://d8j0ntlcm91z4.cloudfront.net/user_33B38CK6rN3xZGtZllRALJXPhHT/hf_20260911_142524_7b6f5bfa-17ef-432f-ad53-2014f1deba21.png",
+ "sambal-udang":"https://d8j0ntlcm91z4.cloudfront.net/user_33B38CK6rN3xZGtZllRALJXPhHT/hf_20260911_142524_8f186f1a-1efe-4965-bc37-0e86277900c7.png",
+ "pad-kra-pao":"https://d8j0ntlcm91z4.cloudfront.net/user_33B38CK6rN3xZGtZllRALJXPhHT/hf_20260911_142525_1d965cd1-f404-46e9-867f-b8c02b7c0be5.png",
+ "chicken-cacciatore":"https://d8j0ntlcm91z4.cloudfront.net/user_33B38CK6rN3xZGtZllRALJXPhHT/hf_20260911_142524_672f038b-ecec-41c5-8ef6-dcfc7ab929c7.png",
+ "thai-green-chicken":"https://d8j0ntlcm91z4.cloudfront.net/user_33B38CK6rN3xZGtZllRALJXPhHT/hf_20260911_142524_bdf391b2-e5bc-43db-b765-2a69b82ccacf.png",
+ "mustard-mushroom-chicken":"https://d8j0ntlcm91z4.cloudfront.net/user_33B38CK6rN3xZGtZllRALJXPhHT/hf_20260911_142532_2ef76f06-9244-4a5d-816f-00404f6c0961.png"
+};
