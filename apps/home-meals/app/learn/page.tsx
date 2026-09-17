243f89a5854e650aac1f403a1fc7b0fc6be5a5e9
Josh G <129348785+Josh-Gi3r@users.noreply.github.com>
2026-09-18 02:38:01 +0800

    Home Meals V20: expose Josh personality memory and drafts in More

--- apps/home-meals/app/learn/page.tsx
+++ apps/home-meals/app/learn/page.tsx
@@ -1,4 +1,5 @@
 import {Help} from "@/components/app/Help";
 import {HouseholdStatusCard} from "@/components/HouseholdStatusCard";
 import {HouseholdMemory} from "@/components/HouseholdMemory";
-export default function HelpPage(){return <><Help/><div className="hm-screen" style={{paddingTop:0}}><HouseholdMemory/><HouseholdStatusCard/></div></>}
+import {JoshMemorySettings} from "@/components/JoshMemorySettings";
+export default function HelpPage(){return <><Help/><div className="hm-screen" style={{paddingTop:0}}><JoshMemorySettings/><HouseholdMemory/><HouseholdStatusCard/></div></>}
